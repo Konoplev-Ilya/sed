41efa8bdbbf7b314730c697c9b493696e8b28374
Illya Konoplev <xucnud@gmail.com>
2021-09-13 12:32:54 +0300

    notify only about increasing mails

--- notification_to_telegram.py
+++ notification_to_telegram.py
@@ -14,20 +14,29 @@ async def check_and_notify(login, passwd, user_id, telegram_token, region):
         session = aiohttp.ClientSession(cookie_jar=jar)
         async with session.post(f'http://198.19.0.6/csp/{region}/wui.main.cls', data = {'CacheUserName' : login, 'CachePassword' : passwd}) as response:
             pass
-        data = None
+        prev_data = None
         while True:
             async with session.get(f'http://198.19.0.6/csp/{region}/json.doc.cls', data = {'oper':'getPersonalData'}) as response:
-                mails = json.loads(await response.text())
-                if data == None:
-                    text_message = f'Инициализация.\nПослупивших: {mails["pcrec"]},\nНа исполнении: {mails["pcexe"]},\nНа согласовании: {mails["pcagree"]},\nНа утверждении: {mails["pcapprove"]}.\nЕсли количество писем возрастет, я сообщу.'
+                mails = list(map(int, (json.loads(await response.text())).values()))
+                if prev_data == None:
+                    text_message = f'Инициализация.\nПоступивших: {mails[0]},\nНа исполнении: {mails[1]},\nНа согласовании: {mails[2]},\nНа утверждении: {mails[3]}.\nЕсли количество писем возрастет, я сообщу.'
                     async with session.post(f'https://api.telegram.org/bot{telegram_token}/sendMessage', data = {'chat_id' : user_id, 'text': text_message}) as r:
                         pass
-                    data = mails
-                elif data != mails:
-                    text_message = f'Изменения.\nПослупивших: {mails["pcrec"]},\nНа исполнении: {mails["pcexe"]},\nНа согласовании: {mails["pcagree"]},\nНа утверждении: {mails["pcapprove"]}.\n'
+                    prev_data = mails
+                elif prev_data != mails:
+                    if mails[0] - prev_data[0] > 0:
+                        text_message = f'Изменения.\nПоступивших: +{mails[0] - prev_data[0]},\n'
+                    if mails[1] - prev_data[1] > 0:
+                        text_message += f'Изменения.\nНа исполнении: +{mails[1] - prev_data[1]},\n'
+                    if mails[2] - prev_data[2] > 0:
+                        text_message += f'Изменения.\nНа согласовании: +{mails[2] - prev_data[2]},\n'
+                    if mails[3] - prev_data[3] > 0:
+                        text_message += f'Изменения.\nНа утверждении: +{mails[3] - prev_data[3]},\n'
+
+                    #text_message = f'Изменения.\nПоступивших: {mails["0"]},\nНа исполнении: {mails["1"]},\nНа согласовании: {mails["2"]},\nНа утверждении: {mails["3"]}.\n'
                     async with session.post(f'https://api.telegram.org/bot{telegram_token}/sendMessage', data = {'chat_id' : user_id, 'text': text_message}) as r:
                         pass
-                    data = mails
+                    prev_data = mails
                 print(login, mails)
                 await asyncio.sleep(5)
 
